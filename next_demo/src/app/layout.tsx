34cf7fa8e315ae3a69b2fe8e4dff9f8da356d536
nkumnick <nkumnick@oev.de>
2023-07-07 10:52:33 +0200

    change fonts

--- next_demo/src/app/layout.tsx
+++ next_demo/src/app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Comic_Neue } from 'next/font/google'
 import Head from "next/head";
 import Card from "@/components/Card";
 import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Comic_Neue({ subsets: ['latin'], weight: '400'})
 
 export const metadata = {
   title: 'Astro vs Next.js Vergleich. Kandidat Next',
